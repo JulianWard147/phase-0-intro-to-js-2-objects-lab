b6a10c9c4a79ae715882a373235a9d6cfe679eae
Julian Ward <wardjulianm@gmail.com>
2023-01-23 17:54:39 -0700

    commit tree 3fa60cac8e059d74cf3fbd89cd4eaa20436fa01d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+const employee = {name : "Julian", streetAddress: "1234 Dog Street"};
+
+function updateEmployeeWithKeyandValue(employee, key, value) {
+    const newRecord = {...employee}
+    newRecord[key] = value
+    return newRecord
+};
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newRecord = {...employee}
+    delete newRecord.key
+    return newRecord
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.key
+};
