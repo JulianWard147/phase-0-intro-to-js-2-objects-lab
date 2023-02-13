d97bc13b242e4790a2c0fe89bcc8d1be5965eecc
Julian Ward <wardjulianm@gmail.com>
2023-02-13 09:50:48 -0700

    Finishing lab for phase 1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+const employee = {"name" : "Julian", "streetAddress": "1234 Dog Street"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newRecord = {...employee}
+    newRecord[key] = value
+    return newRecord
+};
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newRecord = {...employee}
+    delete newRecord[key]
+    return newRecord
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+};
